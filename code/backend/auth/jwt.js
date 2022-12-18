7739d10b21e1a6c898a34a278a633303e619cb55
piumal1999 <piumalrathnayake@hotmail.com>
2022-12-19 02:56:18 +0530

    Add jwt authentication to endpoints

---
+++ code/backend/auth/jwt.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken');
+
+function generateAccessToken(user_id) {
+  return jwt.sign(user_id, process.env.TOKEN_SECRET, { expiresIn: '1800s' });
+}
+
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers['authorization']
+  const token = authHeader && authHeader.split(' ')[1]
+
+  if (token == null) return res.sendStatus(401)
+
+  jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403)
+
+    req.user = user
+    next()
+  })
+}
+
+module.exports = { generateAccessToken, authenticateToken };
--- code/backend/models/User.js
+++ code/backend/models/User.js
@@ -31,7 +31,6 @@ const userSchema = new Schema({
 
   phone: {
     type: String,
-    required: true,
     max: 10,
   },
 
