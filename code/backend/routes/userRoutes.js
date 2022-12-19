b73654f5be4291be16c1510d3a4913a5abfe0600
piumal1999 <piumalrathnayake@hotmail.com>
2022-12-19 19:22:15 +0530

    Wire up signup form

--- code/backend/routes/userRoutes.js
+++ code/backend/routes/userRoutes.js
@@ -11,27 +11,45 @@ const router = express.Router();
 
 // signup user
 router.post("/signup", (req, res) => {
-  const newUser = {
+  let newUser = {
     firstName: req.body.firstName,
     lastName: req.body.lastName,
     password: req.body.password, // TODO: encrypt password
     email: req.body.email,
   };
 
-  if (User.findOne({ email: req.body.email })) {
-    res.status(400).send("User already exists!");
-  } else {
-    User.create(newUser, function (err, user) {
-      if (err) {
-        console.log(err);
-        res.status(400).send("Error creating user!");
-      } else {
-        const token = generateAccessToken({ user_id: user._id });
-        user = { ...user._doc, token: token };
-        res.json(user);
-      }
-    });
-  }
+  deviceId = req.body.device;
+  devicePin = req.body.pin;
+
+  User.findOne({
+    email: req.body.email,
+  }).then((user) => {
+    if (user) {
+      res.status(400).send("User already exists!");
+    } else {
+      Device.findById(deviceId, function (err, device) {
+        if (err || !device) {
+          res.status(400).send("Error fetching device!");
+        } else {
+          if (device.pin === devicePin) {
+            newUser = { ...newUser, device: device };
+            User.create(newUser, function (err, user) {
+              if (err) {
+                console.log(err);
+                res.status(400).send("Error creating user!");
+              } else {
+                const token = generateAccessToken({ user_id: user._id });
+                user = { ...user._doc, token: token };
+                res.status(201).json(user);
+              }
+            });
+          } else {
+            res.status(400).send("Incorrect device pin!");
+          }
+        }
+      });
+    }
+  });
 });
 
 // login user
