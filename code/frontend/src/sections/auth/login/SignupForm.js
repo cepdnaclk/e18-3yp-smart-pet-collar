635531632cc5861d736b167903f4e1415f53468d
Piumal Rathnayake <piumal1999@gmail.com>
2022-12-19 15:51:10 +0530

    Create signup form (#45)

--- code/frontend/src/sections/auth/login/SignupForm.js
+++ code/frontend/src/sections/auth/login/SignupForm.js
@@ -1,36 +1,39 @@
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
 // @mui
-import {
-  Stack,
-  IconButton,
-  InputAdornment,
-  TextField,
-} from "@mui/material";
+import { Stack, IconButton, InputAdornment, TextField } from "@mui/material";
 import { LoadingButton } from "@mui/lab";
 // components
 import Iconify from "../../../components/iconify";
-import { useAuth } from "src/hooks/useAuth";
 import axios from "axios";
+import { useAuth } from "src/hooks/useAuth";
+import { useNavigate } from "react-router-dom";
 
 // ----------------------------------------------------------------------
 
-export default function LoginForm() {
+export default function SignupForm() {
+  const [showPassword, setShowPassword] = useState(false);
   const navigate = useNavigate();
   const { login } = useAuth();
 
-  const [showPassword, setShowPassword] = useState(false);
-
-  const handleLogin = async () => {
+  const handleSignup = async () => {
+    let firstName = document.getElementById("fname").value;
+    let lastName = document.getElementById("lname").value;
     let email = document.getElementById("email").value;
     let password = document.getElementById("password").value;
+    let deviceId = document.getElementById("device").value;
+    let devicePin = document.getElementById("pin").value;
+
     try {
-      const result = await axios.post("http://localhost:3001/login", {
+      const result = await axios.post("http://localhost:3001/signup", {
+        firstName: firstName,
+        lastName: lastName,
         email: email,
         password: password,
+        deviceId: deviceId,
+        devicePin: devicePin,
       });
 
-      if (result.status === 200) {
+      if (result.status === 201) {
         login({
           email: email,
           password: password,
@@ -41,7 +44,7 @@ export default function LoginForm() {
       }
     } catch (error) {
       if (error.response.status === 400) {
-        console.log("Invalid credentials");
+        console.log(error.response.message);
       } else {
         console.log("Something went wrong");
       }
@@ -50,8 +53,16 @@ export default function LoginForm() {
 
   return (
     <>
-      <Stack spacing={3}>
+      <Stack spacing={3} marginBottom={3}>
+        <Stack direction={"row"} spacing={3}>
+          <TextField name="fname" id="fname" label="First Name" fullWidth />
+          <TextField name="lname" id="lname" label="Last Name" fullWidth />
+        </Stack>
         <TextField name="email" id="email" label="Email address" />
+        <Stack direction={"row"} spacing={3}>
+          <TextField name="device" id="device" label="Device Id" fullWidth />
+          <TextField name="pin" id="pin" label="Device Pin" />
+        </Stack>
 
         <TextField
           name="password"
@@ -75,26 +86,14 @@ export default function LoginForm() {
         />
       </Stack>
 
-      <Stack
-        direction="row"
-        alignItems="center"
-        justifyContent="space-between"
-        sx={{ my: 2 }}
-      >
-        {/* <Checkbox name="remember" label="Remember me" /> */}
-        {/* <Link variant="subtitle2" underline="hover">
-          Forgot password?
-        </Link> */}
-      </Stack>
-
       <LoadingButton
         fullWidth
         size="large"
         type="submit"
         variant="contained"
-        onClick={handleLogin}
+        onClick={handleSignup}
       >
-        Login
+        Sign up
       </LoadingButton>
     </>
   );
