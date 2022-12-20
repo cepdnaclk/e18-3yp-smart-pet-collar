2fdd42e8a4013305ffeb9b180d40482c02f28e7c
Piumal Rathnayake <piumal1999@gmail.com>
2022-12-20 13:51:03 +0530

    Update code/backend/test/aws-dummy-mqtt-sensor/test.py

--- code/backend/test/aws-dummy-mqtt-sensor/test.py
+++ code/backend/test/aws-dummy-mqtt-sensor/test.py
@@ -7,9 +7,9 @@ import datetime
 # Define ENDPOINT, CLIENT_ID, PATH_TO_CERTIFICATE, PATH_TO_PRIVATE_KEY, PATH_TO_AMAZON_ROOT_CA_1, MESSAGE, TOPIC, and RANGE
 ENDPOINT = "a26wj855ybmd1h-ats.iot.ap-south-1.amazonaws.com"
 CLIENT_ID = "testDevice"
-PATH_TO_CERTIFICATE = "../.././cert/768405eda3d7783091d099fa7744d4a63308d4ed0f246ba50685abc54886a21f-certificate.pem.crt"
-PATH_TO_PRIVATE_KEY = "../.././cert/768405eda3d7783091d099fa7744d4a63308d4ed0f246ba50685abc54886a21f-private.pem.key"
-PATH_TO_AMAZON_ROOT_CA_1 = "../.././cert/AmazonRootCA1.pem"
+PATH_TO_CERTIFICATE = "../../cert/768405eda3d7783091d099fa7744d4a63308d4ed0f246ba50685abc54886a21f-certificate.pem.crt"
+PATH_TO_PRIVATE_KEY = "../../cert/768405eda3d7783091d099fa7744d4a63308d4ed0f246ba50685abc54886a21f-private.pem.key"
+PATH_TO_AMAZON_ROOT_CA_1 = "../../cert/AmazonRootCA1.pem"
 #MESSAGE = "Hello World"
 TOPIC = "/device1/"
 RANGE = 20
